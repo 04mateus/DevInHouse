4c761d480a1121d83fff7abca830ed22e8162e8d
04mateus <mateus.salgado.b@gmail.com>
2022-07-18 20:35:09 -0300

    Semana 3 - Exercício 2

---
+++ semana03/exercicios/ex2.js
@@ -0,0 +1,23 @@
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+];
+
+
+var listaEl = document.getElementById("lista");
+
+const listaCompleta = listaConvidados.forEach((convidados, i) => {
+    console.log(convidados.nome + " " + convidados.sobrenome)
+    var item = document.createElement("li");
+    var nome = listaConvidados[i].nome + " " + listaConvidados[i].sobrenome
+    item.textContent = nome;
+    listaEl.appendChild(item);
+})
